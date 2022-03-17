4c03efb30421a4f4b6adfc24ff1950a552e506e2
bsoulla <baptiste.soullard@u-psud.fr>
2022-03-17 09:42:27 +0100

    test

--- tapis.php
+++ tapis.php
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-
-<head>
-		<meta charset="utf-8">
-		<title>Web Game</title>
-		<meta name="author" content="monsinjon_soullard">
-		<link rel= "stylesheet" href="game.css">
-</head>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="tapis_script.js"></script>
-
-</script>
-
-<html>
-	<body class = "terrain" id = 'background'>
-
-
-		<div class = 'main' id = 'J'>
-			moi
-		</div>
-
-		<div class = 'main' id = 'Ad1'>
-			toto1
-		</div>
-
-		<div class = 'main' id = 'Ad2'>
-			toto2
-		</div>
-
-		<div class = 'main' id = 'Ad3'>
-			toto3
-		</div>
-
-		<div id = "zone-jeu">
-			zone jeu
-
-		</div>
-
-		<div id = "pioche">
-			pioche
-			<img src = "nouveau dossier\carte_Autres_3.png">
-
-		
-		</div>
-	</body>
-</html>
-=======
 <?php
 echo "
 <div class = 'terrain'>
@@ -76,4 +27,3 @@ echo "
 	</div>
 </div>"
 	?>
->>>>>>> decfe537d6997cc482a32e747a0d45c1e7f3b75f
