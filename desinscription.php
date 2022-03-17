3cf78623ca1fd70265e00b82c547b19246f7033e
Touptwi <monsinjon.samuel@gmail.com>
2022-03-17 12:44:14 +0100

    COOKIES

--- desinscription.php
+++ desinscription.php
@@ -6,13 +6,18 @@
 	
 	$file = json_decode(file_get_contents("utilisateurs.json"), true);
 	
+	/*
 	foreach ($file as $key => $value) {
 		if ($value['nom'] == $nom) {
            	unset($file[$key]);
         }
     }
+	*/
 	
+	unset($file[$_COOKIE["id_player"]]);
+
+	setcookie("id_player", "", time() - 3600);
+
 	file_put_contents("utilisateurs.json",json_encode($file,JSON_PRETTY_PRINT));
 
-echo		'<input type="Button" value="Join" onclick="join()">';
 ?>
--- inscription.php
+++ inscription.php
@@ -2,17 +2,19 @@
 
 	$nom = $_GET['nom'];
 	$niveau = $_GET['niveau'];
-	//$numero = $_GET['numero'];
-	
+
+	$id_player = random_int(1,999);
+		
 	$file = json_decode(file_get_contents("utilisateurs.json"), true);
 
-	$file[] = ['nom' => $nom, 'niveau' => $niveau];
+	if(sizeof($file) == 0) {
+		$id_player += 1000;
+	}
+
+	setcookie("id_player", $id_player, time() + 2*3600,);
+
+	$file[$id_player] = ['nom' => $nom, 'niveau' => $niveau];
 	
 	file_put_contents("utilisateurs.json",json_encode($file,JSON_PRETTY_PRINT));
-	
-	//session_start();
-	
-	//$_SESSION['numero'] = $numero; 
-	
-echo		'<input type="Button" value="Leave" onclick="leave()">';
+
 ?>
