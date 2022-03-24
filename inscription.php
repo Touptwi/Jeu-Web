2777666705ade156c84a02a4c8b33bbce923ea9a
Touptwi <monsinjon.samuel@gmail.com>
2022-03-24 11:46:17 +0100

    Check cookies

--- inscription.php
+++ inscription.php
@@ -11,7 +11,7 @@
 		$id_player += 1000;
 	}
 
-	setcookie("id_player", $id_player, time() + 2*3600,"","",true);
+	setcookie("id_player", $id_player, time() + 2*3600);
 
 	$file[$id_player] = ['nom' => strip_tags($nom), 'niveau' => $niveau];
 	
