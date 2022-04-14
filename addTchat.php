67ba5370b33f490ef2f837a8cba7c7101ceaf7e0
Touptwi <monsinjon.samuel@gmail.com>
2022-04-14 12:50:17 +0200

    Fixe tchat and add feature

--- addTchat.php
+++ addTchat.php
@@ -6,10 +6,20 @@
 		
 	$file = json_decode(file_get_contents("partie_".$id_partie.".json"), true);
     $users = json_decode(file_get_contents("utilisateurs.json"), true);
+	$regle_json = json_decode(file_get_contents("fonctions_jeu/regles.json"),true);
 
     $user = $users[$id_partie][$id_player]["nom"];
 
-    array_push($file["tchat"], $user.": ".strip_tags($message));
+	$message_img = explode(" ", strip_tags($message));
+
+	foreach($message_img as $i) {
+        if($i[0] == "\u00a7") {
+			$carte = $regle_json[substr($i, 1)]["image"];
+            $i = "<img src ='cartes_png/".$carte."'>";
+        }
+    }
+
+    array_push($file["tchat"], $user.": ".implode(" ", $message_img));
 	
 	file_put_contents("partie_".$id_partie.".json",json_encode($file,JSON_PRETTY_PRINT));
 ?>
